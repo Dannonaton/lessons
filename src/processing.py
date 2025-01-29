aefdb5158cf824e0d9d1e863c137aec44dfb5364
Aleksander <mail@mail.ru>
2025-01-29 11:33:54 +0300

    Test lint

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,9 @@ from typing import Any, Dict, List, Union
 
 new_data = []
 
-def filter_by_state(data: List[Dict[str, str]], state: str = 'EXECUTED') -> List[Dict[str, Union[str, None]]]:
-    """ Функция, для сортировки по ключу """
+
+def filter_by_state(data: List[Dict[str, str]], state: str = "EXECUTED") -> List[Dict[str, Union[str, None]]]:
+    """Функция, для сортировки по ключу"""
     for i in data:
         if i["state"] == state:
             new_data.append(i)
@@ -11,6 +12,5 @@ def filter_by_state(data: List[Dict[str, str]], state: str = 'EXECUTED') -> List
 
 
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
-    """ Функция, для сортировки списка по дате """
+    """Функция, для сортировки списка по дате"""
     return sorted(data, key=lambda x: x["date"], reverse=reverse)
-

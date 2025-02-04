0d971d2f5aeeef67ca05bfb51bce402f8a7faedc
Aleksander <mail@mail.ru>
2025-02-04 13:57:17 +0300

    edit lint

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Union
 from src import widget
 
 
@@ -7,12 +8,10 @@ from src import widget
     ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
     ('Счет 64686473678894779589', 'Счет **9589')
 ])
-def test_mask_account_card(type_and_number, expected):
+def test_mask_account_card(type_and_number: Union[str], expected: Union[str]) -> None:
     assert widget.mask_account_card(type_and_number) == expected
 
 
-
-@pytest.mark.parametrize("date, expected",[('2024-03-11T02:26:18.671407', '11.03.2024')])
-
-def test_get_date(date, expected):
+@pytest.mark.parametrize("date, expected", [('2024-03-11T02:26:18.671407', '11.03.2024')])
+def test_get_date(date: Union[str], expected: Union[str]) -> None:
     assert widget.get_date(date) == expected

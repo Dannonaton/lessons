99df78a3808fe3dc54a35b12e81efd913d97d435
Aleksander <mail@mail.ru>
2025-02-04 13:57:17 +0300

    edit lint

--- src/widget.py
+++ src/widget.py
@@ -19,8 +19,7 @@ def mask_account_card(type_and_number: Union[str]) -> str:
         new_input = "".join(split_input[-1])
         return f"{split_input[0]} {split_input[1]} {get_mask_card_number(new_input)}"
     else:
-        print("Вы ввели неккоректные данные")
-
+        return "Вы ввели неккоректные данные"
 
 
 def get_date(user_date: Union[str]) -> str:
@@ -30,5 +29,3 @@ def get_date(user_date: Union[str]) -> str:
     date_format = datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
     new_date = date_format.strftime("%d.%m.%Y")
     return new_date
-
-
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 import pytest
-from src import  masks
 
+from typing import Union
+from src import masks
 
 
 @pytest.mark.parametrize("date, expected", [
@@ -8,14 +9,14 @@ from src import  masks
     ('1234567887654321', '1234 56** **** 4321'),
     ('7865273968278382', '7865 27** **** 8382')
 ])
-def test_get_mask_card_number(date, expected):
+def test_get_mask_card_number(date: Union[str], expected: Union[str]) -> None:
     assert masks.get_mask_card_number(date) == expected
+
 
 @pytest.mark.parametrize("date, expected", [
     ('73654108430135874305', '**4305'),
     ('7123131231313123123123', '**3123'),
     ('13123131231232134124234', '**4234')
 ])
-def test_get_mask_account(date, expected):
+def test_get_mask_account(date: Union[str], expected: Union[str]) -> None:
     assert masks.get_mask_account(date) == expected
-

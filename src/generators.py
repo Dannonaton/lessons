ad5774f7658cd263235ab6eb61f1251c1be2684f
Aleksander <mail@mail.ru>
2025-02-10 16:32:37 +0300

    Написан модуль generators.py и test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-
+from typing import Generator, List, Dict, Any
 
 def filter_by_currency(transactions, currency):
     """Функция принимает на вход список со словарем и возвращает id операции"""
@@ -7,8 +7,91 @@ def filter_by_currency(transactions, currency):
             yield transaction
 
 
-def transaction_descriptions():
-    pass
+def transaction_descriptions(list_of_transactions: List[Dict[str, Any]]) -> Generator[str, None, None]:
+    """Генератор, принимающий список словарей с транзакциями и возвращающий описание каждой операции по очереди"""
+    if not isinstance(list_of_transactions, list):
+        raise TypeError('Ошибка типа данных')
+    for transaction in list_of_transactions:
+        yield transaction["description"]
 
-def card_number_generator():
-    pass
+
+
+def card_number_generator(start: int, stop: int) -> Generator[str,  None, None]:
+    """Генератор номеров банковских карт в формате ХХХХ ХХХХ ХХХХ ХХХХ"""
+    if not isinstance(start, int) or not isinstance(stop, int):
+        raise TypeError('Ошибка типа данных')
+    if stop < start:
+        raise ValueError('Граница диапазона указана не верно(в обратном порядке)')
+    if len(str(start)) <= 16:
+        for number in range(start, stop +1):
+            formated_card_number = "{:016}".format(number)
+            yield " ".join(formated_card_number[i * 4 : ( i + 1) * 4] for i in range(4))
+    else:
+        raise ValueError('Входные данные превышают допустимые значения')
+
+
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {"name": "USD", "code": "USD"},
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {"name": "USD", "code": "USD"},
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {
+            "amount": "43318.34",
+            "currency": {"name": "руб.", "code": "RUB"},
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {
+            "amount": "56883.54",
+            "currency": {"name": "USD", "code": "USD"},
+        },
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+            "amount": "67314.70",
+            "currency": {"name": "руб.", "code": "RUB"},
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
+]
+
+
+print(list(transaction_descriptions(transactions)))
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -41,3 +41,51 @@ def test_filter_by_currency(transactions, currency):
     filtered_transactions = generators.filter_by_currency(transactions, currency)
     assert next(filtered_transactions)["id"] == 939719570
 
+
+@pytest.mark.parametrize('start, stop, expected', [(10, 12, ["0000 0000 0000 0010",
+                                                             "0000 0000 0000 0011",
+                                                             "0000 0000 0000 0012"]),
+                                                   (5, 6, ["0000 0000 0000 0005",
+                                                           "0000 0000 0000 0006"]),
+                                                   (1000, 1002, ["0000 0000 0000 1000",
+                                                                 "0000 0000 0000 1001",
+                                                                 "0000 0000 0000 1002"])])
+def test_card_number_generator(start, stop, expected):
+    result = generators.card_number_generator(start, stop)
+    assert next(result) == expected[0]
+    assert next(result) == expected[1]
+
+
+def test_card_number_generator_wrong_type():
+    with pytest.raises(TypeError):
+        next(generators.card_number_generator([1, 2, 3], 1))
+    with pytest.raises(TypeError):
+        next(generators.card_number_generator(5, "some_string"))
+
+
+def test_card_number_generator_boundary_values():
+    result_max = generators.card_number_generator(9999999999999998, 9999999999999999)
+    assert next(result_max) == "9999 9999 9999 9998"
+    assert next(result_max) == "9999 9999 9999 9999"
+
+
+def test_card_number_generator_after_boundary_values():
+    with pytest.raises(ValueError):
+        next(generators.card_number_generator(10000000000000000, 10000000000000001))
+
+
+def test_card_number_generator_wrong_start_stop():
+    with pytest.raises(ValueError):
+        next(generators.card_number_generator(7, 5))
+
+
+def test_transaction_descriptions_exceptions():
+    with pytest.raises(StopIteration):
+        next(generators.transaction_descriptions([]))
+
+
+def test_transaction_descriptions_wrong_type():
+    with pytest.raises(TypeError):
+        next(generators.transaction_descriptions(1))
+    with pytest.raises(TypeError):
+        next(generators.transaction_descriptions("some_string"))

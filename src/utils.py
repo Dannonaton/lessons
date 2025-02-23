31d92ca8fdbd7427aed3c78b2207a164b9096bb9
aleksandr <aleksander.kryzhanovsky@yandex.ru>
2025-02-23 17:35:13 +0300

    реализована функция Задачи 1

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import json
+import os
+from json import JSONDecodeError
+
+
+def check_operations_file(file_json: str) -> list:
+    """Функция, принимает на вход json file и проверяет его"""
+    try:
+        with open(file_json, 'r') as file:
+            try:
+                transaction = json.load(file)
+            except JSONDecodeError:
+                print('Ошибка файла с транзакциями')
+                return []
+        if not isinstance(transaction, list):
+            print('Список транзакции пуст')
+            return []
+        print('Создан список словарей с данными о фин опер')
+        print(transaction)
+        return transaction
+    except FileNotFoundError:
+        print('Файл не найден')
+        return []
+
+
+
+if __name__ == '__main__':
+    check_operations_file('../data/operations.json')
+    print()

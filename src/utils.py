9991b408483010fd7235a33ac57974b690f183af
aleksandr <aleksander.kryzhanovsky@yandex.ru>
2025-02-23 22:22:37 +0300

    переделка модулей utils.py и test_utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,15 @@
 import json
-from json import JSONDecodeError
+import os.path
 
 
-def check_operations_file(file_json) -> list:
+def check_operations_file(path) -> list:
     """Функция, принимает на вход json file и проверяет его"""
-    try:
-        with open(file_json) as file:
-            try:
-                transaction = json.load(file)
-            except JSONDecodeError:
-                print('Ошибка файла с транзакциями')
-                return []
-        if not isinstance(transaction, list):
-            print('Список транзакции пуст')
-            return []
-        print('Создан список словарей с данными о фин опер')
-        print(transaction)
-        return transaction
-    except FileNotFoundError:
-        print('Файл не найден')
+    if not os.path.isfile(path) or os.path.getsize(path) == 0:
         return []
+    with open(path, 'r', encoding='utf-8') as file:
+        transaction_file = json.load(file)
+        if not isinstance(transaction_file, list):
+            return []
+        else:
+            return transaction_file
+

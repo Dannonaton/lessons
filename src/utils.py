d86f73b1a59c1a3775d209055967233b8a913991
Aleksander <mail@mail.ru>
2025-02-24 14:09:14 +0300

    Создан модуль logger.py, настроено логирование в masks.py и utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,25 @@
 import json
-import os.path
+import os
+
+from src.logger import get_logger
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path_1 = os.path.join(current_dir, '../logs', 'utils.log')
+logger = get_logger('utils.log', file_path_1)
 
 
 def check_operations_file(path) -> list:
     """Функция, принимает на вход json file и проверяет его"""
     try:
+        logger.info(f'Запущена функция {check_operations_file.__name__} и ожидает на вход JSON файл')
         with open(path, 'r', encoding='utf-8') as file:
             transaction_file = json.load(file)
     except (json.JSONDecodeError, FileNotFoundError):
+        logger.error(f'Произошла ошибка типа {FileNotFoundError}, {json.JSONDecodeError}')
         return []
 
     if not isinstance(transaction_file, list):
+        logger.error('Произошла ошибка, JSON-файл не принадлежит типу list')
         return []
-
+    logger.info(f'Функция {check_operations_file.__name__} завершила работу')
     return transaction_file
